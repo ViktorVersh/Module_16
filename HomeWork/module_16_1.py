e524abc22efe9b6cf813ed6be7a70ac7e5abddda
ViktorVersh <vershininzaig@mail.ru>
2025-01-21 02:29:56 +0800

    Модуль 16 задание 1

--- HomeWork/module_16_1.py
+++ HomeWork/module_16_1.py
@@ -1,26 +1,45 @@
 """
 Задача начало пути
 """
-from fastapi import FastAPI
+from fastapi import FastAPI  # Импортируем из библиотеки fastapi класс FastAPI
 
-app = FastAPI()
+app = FastAPI() # Создаем экземпляр класса FastAPI
 
 
 @app.get("/")
 async def home_page():
+    """
+    Функция выводит главную страницу
+    :return:
+    """
     return "Главная страница"
 
 
 @app.get("/user/admin")
 async def user_admin():
+    """
+    Функция выводит страницу администратора
+    :return:
+    """
     return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
 async def user_id(user_id: int):
+    """
+    Функция выводит страницу пользователя
+    :param user_id:
+    :return:
+    """
     return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get("/user")
 async def user_info(username: str = "Илья", age: int = 24):
+    """
+    Функция выводит информацию о пользователе
+    :param username:
+    :param age:
+    :return:
+    """
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
