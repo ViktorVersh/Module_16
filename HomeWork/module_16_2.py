be3fabb134edc82dc9a1828e7691e12b64fa5c5d
ViktorVersh <vershininzaig@mail.ru>
2025-01-21 02:30:18 +0800

    Модуль 16 задание 2

---
+++ HomeWork/module_16_2.py
@@ -0,0 +1,54 @@
+"""
+Аннотация и валидация
+"""
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_home_page():
+    """
+    Функция выводит главную страницу
+    :return:
+    """
+    return "Главная страница"
+
+
+@app.get("/user/admin")
+async def get_user_admin():
+    """
+    Функция выводит страницу администратора
+    :return:
+    """
+    return "Вы вошли как администратор"
+
+
+@app.get("/user/{user_id}")
+async def get_user_id(
+        user_id: Annotated[
+            int, Path(ge=1, le=100, description="Enter User ID", example="10")
+        ]
+):
+    """
+    Функция выводит страницу пользователя
+    :param user_id:
+    :return:
+    """
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get("/user/{username}/{age}")
+async def get_user_info(username: Annotated[
+    str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+                        age: Annotated[
+                            int, Path(ge=18, le=120, description="Enter age", example="24")]):
+    """
+    Функция выводит информацию о пользователе
+    :param username:
+    :param age:
+    :return:
+    """
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
