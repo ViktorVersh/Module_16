9a8b5186dfe6041af4bc72b0988787d51f49f7a0
ViktorVersh <vershininzaig@mail.ru>
2025-01-21 01:49:15 +0800

    Модуль 16 лекция 3

--- Lesson/main_2.py
+++ Lesson/main_2.py
@@ -16,9 +16,11 @@ async def id_paginator(username: str = "Alex", age: int = 34) -> dict:
 
 @app.get("/user/{username}/{id}")
 async def news(
-        username: Annotated[str, Path(min_length=3, max_length=15,description="Введите свое имя", example="montes")],
-        id: Annotated[int, Path(ge=0, le=100, description="Введите свой id", example=75)]
+        username: Annotated[
+            str, Path(min_length=3, max_length=15, description="Введите свое имя", example="montes")
+        ],
+        id: Annotated[
+            int, Path(ge=0, le=100, description="Введите свой id", example=75)
+        ]
 ) -> dict:
     return {"message": f"Hello {username} {id}"}
-
-
