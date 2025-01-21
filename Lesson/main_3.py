4605f4aee157d007542580bd5d79ef92d3c1b1a3
ViktorVersh <vershininzaig@mail.ru>
2025-01-21 14:07:06 +0800

    Модуль 16 лекция 4

---
+++ Lesson/main_3.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+
+app = FastAPI()
+
+message_db = {'0': "First post in FastApi"}
+
+@app.get("/")
+async def get_all_message() -> dict:
+    return message_db
+
+@app.get("/message/{message_id}")
+async def get_message(message_id: str) -> dict:
+    return message_db[message_id]
+
+@qapp.post("/message")
+async def create_message(message: str) -> dict:
+    curent_index = len(int(max(message_db, key=int)) + 1)
+    message_db[curent_index] = message
+    return "Message created"
+
+@app.put("/message/{message_id}")
+async def update_message(message_id: int, message: sstr) -> str:
+    message_db[message_id] = message
+    return "Message updated."
+
+@app.delete("/message/{message_id}")
+async def delete_message(message_id: str) -> str:
+    message_db.pop(message_id)
+    return f"Message with {message_id} was deleted."
+
+@app.delete("/")
+async def delete_all_message() -> str:
+    message_db.clear()
+    return "All messages deleted."
